62c40bf09f9d2bd6023efc60646e8431beb70957
Andre Lanna <andrelanna@alpml>
2020-07-09 16:55:08 -0300

    The script for perform evolution-aware analysis is ready.
    evolutionAwareAnalysis.sh is the file containing the script for performing
    evolution-aware analysis.
    
    The script varies according to the parameters informed when it is called. The
    parameters (and their values) are:
    
    --spl <<spl_name>> : the SPL under analysis. (Default: BSN.)
    --initial_evolution <<number>>: the spl initial version. This version will be
    	analyzed only once so it creates the first results which will be reused
            by next analyses.  (Default: 0)
    --final_evolution <<number>>: the last spl version that will be analyzed.
            (Default: 5)
    --iterations <<number>>: the number of analyses for each spl evolution.
            (Default: 3)
    --xms <<size>>: specifies the initial memory allocation pool for JVM. (Default:
            1024m)
    --xmx <<size>>: specifies the maximum memory allocation pool for JVM. (Default:
            15360m)

---
+++ evolutionAwareAnalysis.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Default values
+xms=1024m
+xmx=15360m
+spl=${spl:-BSN}
+initial_evolution=0
+final_evolution=5
+iterations=3
+
+# Usage mode ./evolutionAwareAnalysis <<spl>> <<initial_evolution>> <<final_evolution>> <<iterations>> <<persist>> <<xms>> <<xmx>>
+# Declare parameters and assign their values.
+
+while [ $# -gt 0 ]; do 
+	if [[ $1 == *"--"* ]]; then
+		param="${1/--/}"
+		declare $param="$2"
+	fi
+
+	shift
+done
+
+
+
+# 1st step: perform the analysis of the original spl
+echo "1st step - analysis of the original spl"
+rm -r AnalysisLog
+rm *.add
+rm -r ADDS
+
+mkdir AnalysisLog
+
+java -Xms$xms -Xmx$xmx -jar reanaEOrdering.jar --all-configurations --uml-model=$spl/bm$spl$initial_evolution.xml --feature-model=$spl/fm$spl$initial_evolution.txt
+
+
+# 2nd step: perform the analysis of the evolutions
+
+for i in $(seq 1 $iterations ); do 
+	for e in $(seq $(expr $initial_evolution + 1) $final_evolution); do
+		echo ----------   Iteration $i     Evolution $e   ---------- 
+		java -Xms$xms -Xmx$xmx -jar reanaEOrdering.jar --all-configurations --uml-model=$spl/bm$spl$e.xml -feature-model=$spl/fm$spl$e.txt >> AnalysisLog/evolution$e.out
+	done
+done
