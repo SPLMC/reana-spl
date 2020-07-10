a22ee01cc2741b14c0a86579c5085acbb1b236dd
Andre Lanna <andre.lanna@gmail.com>
2020-07-10 13:50:23 -0300

    The script for evolution-aware analyses now report the analysis and total times for all iterations in AnalysisLog/analysisTime.out and AnalysisLog/totalTime.out files.

--- evolutionAwareAnalysis.sh
+++ evolutionAwareAnalysis.sh
@@ -24,7 +24,7 @@ done
 
 # 1st step: perform the analysis of the original spl
 echo "1st step - analysis of the original spl"
-rm -r AnalysisLog
+rm -r AnalysisLog*
 rm *.add
 rm -r ADDS
 
@@ -37,7 +37,18 @@ java -Xms$xms -Xmx$xmx -jar reanaEOrdering.jar --all-configurations --uml-model=
 
 for i in $(seq 1 $iterations ); do 
 	for e in $(seq $(expr $initial_evolution + 1) $final_evolution); do
+		mkdir AnalysisLog$i
 		echo ----------   Iteration $i     Evolution $e   ---------- 
-		java -Xms$xms -Xmx$xmx -jar reanaEOrdering.jar --all-configurations --uml-model=$spl/bm$spl$e.xml -feature-model=$spl/fm$spl$e.txt >> AnalysisLog/evolution$e.out
+		java -Xms$xms -Xmx$xmx -jar reanaEOrdering.jar --all-configurations --uml-model=$spl/bm$spl$e.xml -feature-model=$spl/fm$spl$e.txt >> AnalysisLog$i/evolution$e.out
+	done
+done
+
+# 3rd step: recover all analyses and total times of all evolutions
+for e in $(seq $(expr $initial_evolution + 1) $final_evolution); do
+	echo ---------- Evolution $e ---------- >> AnalysisLog/analysisTime.out
+	echo ---------- Evolution $e ---------- >> AnalysisLog/totalTime.out
+	for i in $(seq 1 $iterations ); do
+		cat AnalysisLog$i/evolution$e.out | grep analysis | awk '{print $4}' >> AnalysisLog/analysisTime.out	
+		cat AnalysisLog$i/evolution$e.out | grep total | awk '{print $4}' >> AnalysisLog/totalTime.out	
 	done
 done
