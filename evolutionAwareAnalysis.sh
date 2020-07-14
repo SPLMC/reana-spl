b1ff38ffb62cfd794821bc99a8374ceac544bd57
Thiago Castro <thiago.mael@gmail.com>
2020-07-13 22:15:54 -0300

    Allows running experiments with the working build instead of JAR files
    Previously, the script for running experiments was hard-coded to run a
    specific JAR file. Now, one can simply point to the directory where
    their IDE puts compiled classes and run experiments without generating a
    new JAR everytime.
    
    Also, reports are now persisted in individual directories per SPL.

--- evolutionAwareAnalysis.sh
+++ evolutionAwareAnalysis.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Runtime configuration
+LIBS="libs/*"
+CLASSES="out/production/reana-spl"  # Change this to the path where your IDE puts compiled classes
+CLASSPATH=$CLASSES:$LIBS
+
 # Default values
 xms=1024m
 xmx=15360m
@@ -20,35 +25,40 @@ while [ $# -gt 0 ]; do
 	shift
 done
 
-
+COMMAND="CLASSPATH=$CLASSPATH java -Xms$xms -Xmx$xmx ui.CommandLineInterface"
+# Path were we dump and read ADDs for previous analyses
+PERSISTED_ANALYSES_PATH=Analyses/$spl
+# Path were we save analysis stats
+LOGS_DIR=$PERSISTED_ANALYSES_PATH/logs
 
 # 1st step: perform the analysis of the original spl
 echo "1st step - analysis of the original spl"
-rm -r AnalysisLog*
-rm *.add
-rm -r ADDS
+rm -rf $PERSISTED_ANALYSES_PATH
+# TODO: we have to parameterize the variableStore's location
+rm variableStore.add
 
-mkdir AnalysisLog
+mkdir -p $PERSISTED_ANALYSES_PATH
+mkdir -p $LOGS_DIR
 
-java -Xms$xms -Xmx$xmx -jar reanaEOrdering.jar --all-configurations --uml-model=$spl/bm$spl$initial_evolution.xml --feature-model=$spl/fm$spl$initial_evolution.txt
+eval "$COMMAND --all-configurations --uml-model=$spl/bm$spl$initial_evolution.xml --feature-model=$spl/fm$spl$initial_evolution.txt --persisted-analyses=$PERSISTED_ANALYSES_PATH"
 
 
 # 2nd step: perform the analysis of the evolutions
 
 for i in $(seq 1 $iterations ); do 
 	for e in $(seq $(expr $initial_evolution + 1) $final_evolution); do
-		mkdir AnalysisLog$i
+		mkdir -p $LOGS_DIR/$i
 		echo ----------   Iteration $i     Evolution $e   ---------- 
-		java -Xms$xms -Xmx$xmx -jar reanaEOrdering.jar --all-configurations --uml-model=$spl/bm$spl$e.xml -feature-model=$spl/fm$spl$e.txt >> AnalysisLog$i/evolution$e.out
+		eval "$COMMAND --all-configurations --uml-model=$spl/bm$spl$e.xml --feature-model=$spl/fm$spl$e.txt --persisted-analyses=$PERSISTED_ANALYSES_PATH >> $LOGS_DIR/$i/evolution$e.out"
 	done
 done
 
 # 3rd step: recover all analyses and total times of all evolutions
 for e in $(seq $(expr $initial_evolution + 1) $final_evolution); do
-	echo ---------- Evolution $e ---------- >> AnalysisLog/analysisTime.out
-	echo ---------- Evolution $e ---------- >> AnalysisLog/totalTime.out
+	echo ---------- Evolution $e ---------- >> $LOGS_DIR/analysisTime.out
+	echo ---------- Evolution $e ---------- >> $LOGS_DIR/totalTime.out
 	for i in $(seq 1 $iterations ); do
-		cat AnalysisLog$i/evolution$e.out | grep analysis | awk '{print $4}' >> AnalysisLog/analysisTime.out	
-		cat AnalysisLog$i/evolution$e.out | grep total | awk '{print $4}' >> AnalysisLog/totalTime.out	
+		cat $LOGS_DIR/$i/evolution$e.out | grep "Total analysis" | awk '{print $4}' >> $LOGS_DIR/analysisTime.out
+		cat $LOGS_DIR/$i/evolution$e.out | grep "Total running" | awk '{print $4}' >> $LOGS_DIR/totalTime.out
 	done
 done
